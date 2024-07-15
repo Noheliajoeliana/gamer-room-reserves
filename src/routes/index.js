becedd5c98aac7fe02718f414374e5ed2853dc6a
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2024-07-15 15:22:39 -0400

    feat: remove reservation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const registerUser = require('./register');
 const login = require('./login');
-const { getReservations, postReservation } = require('./reservation');
+const { getReservations, postReservation, removeReservation } = require('./reservation');
 
 module.exports = {
   registerUser,
   login,
   getReservations,
-  postReservation
+  postReservation,
+  removeReservation
 }
