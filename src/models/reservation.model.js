5a46b2b6ef66f6e1a7a9d4722be974d48c1d1a37
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2024-07-15 14:33:20 -0400

    fix: reservation schema to match requirements

--- src/models/reservation.model.js
+++ src/models/reservation.model.js
@@ -14,6 +14,16 @@ const Reservation = new Schema({
     type: Number,
     required: true
   },
+  startWeek: {
+    type: Number
+  },
+  endWeek: {
+    type: Number
+  },
+  title: {
+    type: String,
+    maxLength: 100
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
